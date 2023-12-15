f9d46d66b7c0f5dceb846761c4ea6f1a35837711
Greentaylor27 <7196@holbertonstudents.com>
2023-12-15 10:14:21 -0800

    Adding libraries to header file

--- monty.h
+++ monty.h
@@ -31,6 +31,10 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
