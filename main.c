b2dd303d984a97d845ea25200c09f3214aeb0cb6
Greentaylor27 <7196@holbertonstudents.com>
2023-12-15 10:25:29 -0800

    Start of the Main.c file

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * main - always checks for success
+ * @argc: Arguement count
+ * @argv: Arguement vector
+ * Return: EXIT_SUCCESS on success
+ * EXIT_FAILURE on failure
+ */
+
+int main(int *argc, char *argv[])
+{
+
